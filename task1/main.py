6b1e6e4b8c89efbca5cfcc85ff41beca1c141cb1
ishaan-ansari <ishaanansari044@gmail.com>
2025-11-05 01:54:00 +0530

    task 1 initial commit

---
+++ task1/main.py
@@ -0,0 +1,7 @@
+"""
+FastAPI Application for Task 1 - Method Statement Automation
+
+- Use the BOQ Extract and Project Details Sheet to generate a draft Method Statement for blockwork & plastering.
+- Populate at least the following sections: Scope, References, Materials, Work Procedure.
+"""
+
