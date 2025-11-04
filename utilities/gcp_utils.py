00c16b9599b2643a1d65d625a0641327c646133b
ishaan-ansari <ishaanansari044@gmail.com>
2025-11-05 02:03:29 +0530

    implemented gcp utils

--- utilities/gcp_utils.py
+++ utilities/gcp_utils.py
@@ -3,3 +3,22 @@ from typing import Optional
 from google.cloud import vision
 from google.oauth2 import service_account
 
+from config import GOOGLE_CREDENTIALS_JSON_FILEPATH
+from logger import loggerUtils as logger
+
+try:
+    GCP_CREDENTIALS = service_account.Credentials.from_service_account_file(
+        GOOGLE_CREDENTIALS_JSON_FILEPATH
+    )
+except Exception as e:
+    logger.warning(
+        f"GCP credentials could not be loaded from {GOOGLE_CREDENTIALS_JSON_FILEPATH}: {e}"
+    )
+    GCP_CREDENTIALS = None
+
+def get_gcp_vision_client() -> Optional[vision.ImageAnnotatorClient]:
+    if GCP_CREDENTIALS:
+        return vision.ImageAnnotatorClient(credentials=GCP_CREDENTIALS)
+    else:
+        logger.error("GCP credentials are not available.")
+        return None
